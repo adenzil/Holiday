833a6df9f071fb0971a34314a03ee9bea53a4253
alister <alistercabral@gmail.com>
2020-08-14 16:39:51 +0530

    make componenet self reliant

--- pages/Components/CountrySelector.js
+++ pages/Components/CountrySelector.js
@@ -1,12 +1,18 @@
 import { useContext } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectCountry } from '../actions'
 
-export default ({ changeCountry, countries, country }) => {
-  
+export default () => {
+
+  const dispatch = useDispatch()
+
+  const country = useSelector((state) => state.country)
+  const countries = useSelector((state) => state.countries)
+
   return (
     <React.Fragment>
       <h2>Countries</h2>
-      <select onChange={e => changeCountry(e.target.value)} value={country}>
+      <select onChange={e => dispatch(selectCountry(e.target.value))} value={country}>
         <option>Select a country</option>
         {countries.map(country => <option key={country.code} value={country.code}>{country.name}</option>)}
       </select>
